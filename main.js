a99244e33467977f08b68eabcbd9771c6e8869a5
mitchg1983 <mitchg83@Geoffreys-MBP.lan>
2021-06-05 10:42:09 -0400

    Add intro and beginner art

--- main.js
+++ main.js
@@ -11,164 +11,196 @@ let win = 0;
 let posX = 0;
 let posY = 0;
 
-
-//This is a test line, will remove for final version.
-console.log ('Your position is ', posX, ',', posY);
-
+//Starting value for the intro.
+let intro = 0
 
 //Main content of the game. Once inside this while loop, all player input is collected. Whatever is written here will
 //appear each time the code is run. I may want to add a condition where the text here is only displayed the first time
 //a player starts the game.
 
 while (win !== 1) {
-    console.log ('Hello!');
-    direc = prompt ('What direction would you like to travel?');
-
-    //Below here are the four different directions the player can travel. Their location is updated after each run of the code.    
-    if (direc === 'North') {
-        posX = (posX + 1);
-        console.log ('Your position is ', posX, posY);
+    
+    //This small while loop is the intro for the game.
+    while (intro === 0){
+        console.log ('                                   /\\\									 ')
+        console.log ('                              /\\\  //\\\\\\								')
+        console.log ('                       /\\\    //\\\\\\///\\\\\\\\\        /\\\						')
+        console.log ('                      //\\  ///\////\\\\  /\  //\\						')
+        console.log ('         /\\\          /  ^ \\\/^ ^/^  ^  ^ \\\/^ \\\/  ^ \\\					')
+        console.log ('        / ^\\\    /\\\  / ^   /  ^/ ^ ^ ^   ^\\\ ^/  ^^  \\\					')
+        console.log ('       /^   \\\  / ^\\\/ ^ ^   ^ / ^  ^    ^  \\\/ ^   ^  \\\       *			')
+        console.log ('      /  ^ ^ \\\/^  ^\\\ ^ ^ ^   ^  ^   ^   ____  ^   ^  \\\     /|\\\			')
+        console.log ('     / ^ ^  ^ \\\ ^  _\\\___________________|  |_____^ ^  \\\   /||o\\\		')
+        console.log ('    / ^^  ^ ^ ^\\\  /______________________________\\\ ^ ^ \\\ /|o|||\\\		')
+        console.log ('   /  ^  ^^ ^ ^  /________________________________\\\  ^  /|||||o|\\\		')
+        console.log ('  /^ ^  ^ ^^  ^    ||___|___||||||||||||___|__|||      /||o||||||\\\		') 
+        console.log (' / ^   ^   ^    ^  ||___|___||||||||||||___|__|||          | |        	')
+        console.log ('/ ^ ^ ^  ^  ^  ^   ||||||||||||||||||||||||||||||oooooooooo| |ooooooo 	')
+        console.log ('ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo		       	')
+        console.log ('       _________    __  _________   _   _____    __  _________')
+        console.log ('      / ____/   |  /  |/  / ____/  / | / /   |  /  |/  / ____/')
+        console.log ('     / / __/ /| | / /|_/ / __/    /  |/ / /| | / /|_/ / __/   ')
+        console.log ('    / /_/ / ___ |/ /  / / /___   / /|  / ___ |/ /  / / /___   ')
+        console.log ('    \\\____/_/  |_/_/  /_/_____/  /_/ |_/_/  |_/_/  /_/_____/   ')
+        
+        console.log ('Hello! Welcome to the game.');
+        console.log ('Type START to begin.');
+        console.log ('');
+        begin = prompt ('>');
+        if (begin === 'START') {
+            intro = 1;
+        }
     }
-    if (direc === 'South') {
-        posX = (posX - 1);
-        console.log ('Your position is ', posX, posY);
-    }
-    if (direc === 'East') {
-        posY = (posY + 1);
-        console.log ('Your position is ', posX, posY);
-    }
-    if (direc === 'West') {
-        posY = (posY - 1);
-        console.log ('Your position is ', posX, posY);
-    }
-       
+    
+    while (intro === 1) {
+        console.log ('Hello!');
+        direc = prompt ('What direction would you like to travel?');
+        
+        //Below here are the four different directions the player can travel. Their location is updated after each run of the code.    
+        if (direc === 'North') {
+            posX = (posX + 1);
+            console.log ('Your position is ', posX, posY);
+        }
+        if (direc === 'South') {
+            posX = (posX - 1);
+            console.log ('Your position is ', posX, posY);
+        }
+        if (direc === 'East') {
+            posY = (posY + 1);
+            console.log ('Your position is ', posX, posY);
+        }
+        if (direc === 'West') {
+            posY = (posY - 1);
+            console.log ('Your position is ', posX, posY);
+        }
+        
         //Here are the 25 different 'squares' or 'locations' of the game. 
-
+        
         if (posX === 0 && posY === 0 ) {
-
+            
         }
         
-
+        
         if (posX === 1 && posY === 0 ) {
-
-        }
-
-
-        if (posX === 2 && posY === 0 ) {
-
-        }
-
-
-        if (posX === -1 && posY === 0 ) {
-
-        }
-
-
-        if (posX === -2 && posY === 0 ) {
-
-
-        }
-
-        if (posX === 0 && posY === 1 ) {
-
+            
         }
         
-
+        
+        if (posX === 2 && posY === 0 ) {
+            
+        }
+        
+        
+        if (posX === -1 && posY === 0 ) {
+            
+        }
+        
+        
+        if (posX === -2 && posY === 0 ) {
+            
+            
+        }
+        
+        if (posX === 0 && posY === 1 ) {
+            
+        }
+        
+        
         if (posX === 1 && posY === 1 ) {
-
+            
         }
-
-
+        
+        
         if (posX === 2 && posY === 1 ) {
-
+            
         }
-
-
+        
+        
         if (posX === -1 && posY === 1 ) {
-
+            
         }
-
-
+        
+        
         if (posX === -2 && posY === 1 ) {
-
-
+            
+            
         }
         if (posX === 0 && posY === 2 ) {
-
+            
         }
         
-
+        
         if (posX === 1 && posY === 2 ) {
-
+            
         }
-
-
+        
+        
         if (posX === 2 && posY === 2 ) {
-
+            
         }
-
-
+        
+        
         if (posX === -1 && posY === 2 ) {
-
+            
         }
-
-
+        
+        
         if (posX === -2 && posY === 2 ) {
-
-
+            
+            
         }
         if (posX === 0 && posY === -1 ) {
-
+            
         }
         
-
+        
         if (posX === 1 && posY === -1 ) {
-
+            
         }
-
-
+        
+        
         if (posX === 2 && posY === -1 ) {
-
+            
         }
-
-
+        
+        
         if (posX === -1 && posY === -1 ) {
-
+            
         }
-
-
+        
+        
         if (posX === -2 && posY === -1 ) {
-
-
+            
+            
         }
         if (posX === 0 && posY === -2 ) {
-
+            
         }
         
-
+        
         if (posX === 1 && posY === -2 ) {
-
+            
         }
-
-
+        
+        
         if (posX === 2 && posY === -2 ) {
-
+            
         }
-
-
+        
+        
         if (posX === -1 && posY === -2 ) {
-
+            
         }
-
-
+        
+        
         if (posX === -2 && posY === -2 ) {
             win = 1;
-
+            
         }
-
+        
         //End of the 'squares'/'locations' section.
-
-
+    }
+    
 }
 
 if (win === 1) {

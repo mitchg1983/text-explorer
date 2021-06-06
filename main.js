463b23dbf5d277ea72a5c71332534e178edf3a48
mitchg1983 <mitchg83@Geoffreys-MacBook-Pro.local>
2021-06-06 16:42:10 -0400

    Add location descriptions

--- main.js
+++ main.js
@@ -20,12 +20,15 @@ let dog = 0;
 //This is the starting value for the ferry, 0 mneans the player is not in a square where they an access the boat.
 let ferry = 1;
 
+
+locdes = 'A ferryman waits by the river.'
+
 //Main content of the game. Once inside this while loop, all player input is collected. Whatever is written here will
 //appear each time the code is run. I may want to add a condition where the text here is only displayed the first time
 //a player starts the game.
 
 
-
+console.clear ();
 console.log ('                                   /\\\									 ')
 console.log ('                              /\\\  //\\\\								')
 console.log ('                       /\\\    //\\\\///\\\\\\\        /\\\						')
@@ -54,14 +57,23 @@ begin = prompt ('>');
 if (begin === 'START') {
     intro = 1;
 }
+console.clear ();
+console.log ('Press ENTER to continue...');
+prompt ();
+console.log ('You dog Fluffy has gone missing! Explore your surroundings and bring Fluffy back home.');
+console.log ('You are outside your house by the Big Boy River, it is a beautiful day and you can see fish happily swimming in the river.');
+console.log ('A ferryman waits by his boat, type Ferry to cross the river.');
+console.log ('When choosing a direction, type North, South, East or West.');
+prompt ();
+
 
 while (intro === 1 && win === 0) {
     //Now that the intro has been completed, we are in the game 'proper'. 
     
-    console.clear ();
-    console.log ('');
+
+
     console.log ('You are currently at ', posX, posY);
-    console.log ('Ferry =', ferry);
+    console.log (locdes);
     console.log ('What direction would you like to travel?');
     console.log ('');
     direc = prompt ('>');
@@ -87,6 +99,7 @@ while (intro === 1 && win === 0) {
     }
 
     if (direc === 'North' && posY === 3) {
+        console.clear ();
         console.log ('A dense forest blocks your path. Press ENTER to choose a new direction.');
         prompt ('');
         posY = (posY -1);
@@ -110,6 +123,7 @@ while (intro === 1 && win === 0) {
         console.clear ();
     }
     if (direc === 'South' && posY === -3) {
+        console.clear ();
         console.log ('A dense forest blocks your path. Press ENTER to choose a new direction.');
         prompt ('');
         posY = (posY + 1);
@@ -124,6 +138,7 @@ while (intro === 1 && win === 0) {
     }
 
     if (direc === 'East' && posX === 3) {
+        console.clear ();
         console.log ('A dense forest blocks your path. Press ENTER to choose a new direction.');
         prompt ('');
         posX = (posX - 1);
@@ -138,30 +153,41 @@ while (intro === 1 && win === 0) {
     }
     
     if (direc === 'West' && posX === -3) {
+        console.clear ();
         console.log ('A dense forest blocks your path. Press ENTER to choose a new direction.');
         prompt ('');
         posX = (posX + 1);
     }
     
     else if (direc === 'Ferry' && ferry === 1 && posY === 0) {
+        console.clear ();
         console.log ('The ferryman takes you across the river.');
         console.log ('Press ENTER to continue.');
         prompt ('');
         posY = (posY - 1);
     }
     else if (direc === 'Ferry' && ferry === 1 && posY === -1) {
+        console.clear ();
         console.log ('The ferryman takes you across the river.');
         console.log ('Press ENTER to continue.');
         prompt ('');
         posY = (posY + 1);
     }
     
+    else if (direc === 'Ferry' && ferry !== 1) {
+        console.clear ();
+        console.log ('There is no ferry available here.');
+        prompt ();
+    }
+
     //Here are the 25 different 'squares' or 'locations' of the game. 
     
     if (posX === 0 && posY === 0 && dog === 0) {
         console.log ('This is the starting space. You begin and end the game here.');
-        console.log ('There is a ferryman here who can take you across the river. Type (Ferry) to use his boat.')
+        console.log ('There is a ferryman here who can take you across the river. Type Ferry to use his boat.')
         ferry = 1;
+        console.log ('Press ENTER to continue...')
+        prompt ();
     }
     
     if (posX === 0 && posY === 0 && dog === 1) {
@@ -170,68 +196,95 @@ while (intro === 1 && win === 0) {
     }
     
     if (posX === 1 && posY === 0 ) {
-        console.log ('You are in 1,0')
+        ferry = 0;
+        locdes = 'Wide open fields stretch North. The riverbank is steep here and too dangerous to attempt a crossing.';
+        console.log ('Press ENTER to continue...');
+        prompt ();
     }
     
     
     if (posX === 2 && posY === 0 ) {
-        ferry = 0;
+        locdes = 'A large forest lies to the North. Wide open fields stretch further South.';
+        console.log ('Press ENTER to continue...');
+        prompt ();
     }
     
     
     if (posX === -1 && posY === 0 ) {
-        console.log ('You are in -1,0');
         ferry = 0;
-        
+        locdes = 'A ferryman waits by the river.';
+        console.log ('Press ENTER to continue...')
+        prompt ();
     }
     
     
     if (posX === -2 && posY === 0 ) {
-        
+        locdes = 'A large forest lies further South.';
+        console.log ('Press ENTER to continue...');
+        prompt ();
         
     }
     
     if (posX === 0 && posY === 1 ) {
-        
+        locdes = 'Wide open fields surround you. What a beautiful day.';
+        console.log ('Press ENTER to continue...');
+        prompt (); 
     }
     
     
     if (posX === 1 && posY === 1 ) {
-        
+        locdes = 'Here the wide open fields give way to wetlands. The terrain is rough.';
+        console.log ('Press ENTER to continue...');
+        prompt ();
     }
     
     
     if (posX === 2 && posY === 1 ) {
-        
+        locdes = 'Soggy swamps and dead trees fill this area.';
+        console.log ('Press ENTER to continue...')
+        prompt ();
     }
     
     
     if (posX === -1 && posY === 1 ) {
-        
+        locdes = 'Wide open fields stretch as far as the eye can see.';
+        console.log ('Press ENTER to continue...')
+        prompt ();
     }
     
     
     if (posX === -2 && posY === 1 ) {
-        
+        locdes = 'The river disappears into a thick forest to the West.';
+        console.log ('Press ENTER to continue...')
+        prompt ();
         
     }
     if (posX === 0 && posY === 2 ) {
+        locdes = 'Trees surround you and impede your progress as you continue North.';
         ferry = 0;
+        console.log ('Press ENTER to continue...')
+        prompt ();
     }
     
     
     if (posX === 1 && posY === 2 ) {
-        
+        locdes = 'Old trees and thorny bushes have taken over this area.';
+        console.log ('Press ENTER to continue...')
+        prompt ();
     }
     
     
     if (posX === 2 && posY === 2 ) {
-        
+        locdes = 'This is it. The furthest away fromm home, you\'ve ever been.';
+        console.log ('Press ENTER to continue...')
+        prompt ();
     }
     
     
     if (posX === -1 && posY === 2 ) {
-        
+        locdes = 'Not a lot to see here.';
+        console.log ('Press ENTER to continue...')
+        prompt ();
     }
     
     
@@ -270,52 +323,67 @@ console.log (' ');
         
     }
     if (posX === 0 && posY === -1 ) {
-        console.log ('There is a ferryman here who can take you across the river. Type (Ferry) to use his boat.')
+        locdes = 'There is a ferryman here who can take you across the river. Type (Ferry) to use his boat.';
         ferry = 1;
+        console.log ('Press ENTER to continue...')
+        prompt ();
     }
     
     
     if (posX === 1 && posY === -1 ) {
         ferry = 0;
+        locdes = 'You never liked this side of the river.';
+        console.log ('Press ENTER to continue...')
+        prompt ();
     }
     
     
     if (posX === 2 && posY === -1 ) {
-        
+        console.log ('Press ENTER to continue...')
+        prompt ();
     }
     
     
     if (posX === -1 && posY === -1 ) {
-        ferry = 0; 
+        ferry = 0;
+        console.log ('Press ENTER to continue...')
+        prompt (); 
     }
     
     
     if (posX === -2 && posY === -1 ) {
-        
+        console.log ('Press ENTER to continue...')
+        prompt ();
         
     }
     if (posX === 0 && posY === -2 ) {
         ferry = 0;
+        console.log ('Press ENTER to continue...')
+        prompt ();
     }
     
     
     if (posX === 1 && posY === -2 ) {
-        
+        console.log ('Press ENTER to continue...')
+        prompt ();
     }
     
     
     if (posX === 2 && posY === -2 ) {
-        
+        console.log ('Press ENTER to continue...')
+        prompt ();
     }
     
     
     if (posX === -1 && posY === -2 ) {
-        
+        console.log ('Press ENTER to continue...')
+        prompt ();
     }
     
     
     if (posX === -2 && posY === -2 ) {
-        
+        console.log ('Press ENTER to continue...')
+        prompt ();
     }
     
     //End of the 'squares'/'locations' section.
